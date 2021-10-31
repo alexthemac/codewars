a61c08c3ab048aeb4f9122512c09e4435c45be82
Alex MacLure <almaclure@gmail.com>
2021-10-31 21:04:00 +0000

    Fix iteration bux, now iterates through as many times as needed. Kata complete

--- direction-reducer/direction-reducer.js
+++ direction-reducer/direction-reducer.js
@@ -1,72 +1,22 @@
 function dirReduc(arr) {
 
-  // for (const dir of arr) {
-  //   console.log(dir);
+  for (let i = 0; i < arr.length;) {
+    if (arr[i] === "NORTH" && arr[i + 1] === "SOUTH" ||
+        arr[i] === "SOUTH" && arr[i + 1] === "NORTH" ||
+        arr[i] === "EAST" && arr[i + 1] === "WEST" ||
+        arr[i] === "WEST" && arr[i + 1] === "EAST") {
+        
+      arr.splice(i, 2);
 
-  // }
-
-  let newArr = [];
-
-  let dirPair = true;
-
-
-  do {
-
-    dirPair = false;
-
-    for (let i = 0; i < arr.length;) {
-
-      // console.log(arr[i], arr[i+1]);
-      if ((arr[i] === "NORTH" && arr[i + 1] === "SOUTH" || 
-          arr[i] === "SOUTH" && arr[i + 1] === "NORTH" ||
-          arr[i] === "EAST" && arr[i + 1] === "WEST" ||
-          arr[i] === "WEST" && arr[i + 1] === "EAST")) {
-          
-            i = i + 2;
-
-            dirPair = true;
-
-          } else {
-            // console.log("PUSHING", i, arr[i]);
-  
-            newArr.push(arr[i]);
-            i = i + 1;
-            
-          }    
-
-          
-
+      i = 0;
+    } else {
+      i++;
     }
-
-    // console.log("newArr", newArr);
-
-
-    //       console.log("arr, PRE DECON", arr);
-
-
-    //       arr = [...newArr];
-
-    //       console.log("arr, Post DECON", arr);
-
-  } while (dirPair === true)
- 
-
-
-  // arr.forEach((element, index) => {
-  //   if (!(arr[index] === "NORTH" && arr[index + 1] === "SOUTH" || 
-  //       arr[index] === "SOUTH" && arr[index + 1] === "NORTH" ||
-  //       arr[index] === "EAST" && arr[index + 1] === "WEST" ||
-  //       arr[index] === "WEST" && arr[index + 1] === "EAST")) {
-  //         newArr.push(element);
-  //       }
-  //   }
-  // )
-
-  return(newArr);
-
+  }
+  return (arr);
 }
 
 //Test cases
-console.log(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])) //Result: ["WEST"]
-// console.log(dirReduc(["NORTH", "WEST", "SOUTH", "EAST"])) //Result: ["NORTH", "WEST", "SOUTH", "EAST"]
-// console.log(dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"])) //Result: []
+console.log(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])); //Result: ["WEST"]
+console.log(dirReduc(["NORTH", "WEST", "SOUTH", "EAST"])); //Result: ["NORTH", "WEST", "SOUTH", "EAST"]
+console.log(dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"])); //Result: []
